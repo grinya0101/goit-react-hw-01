82e5b929c103e3104f04b1d0fc29ec764e6dacca
grinya0101 <grinya1994015@gmail.com>
2023-03-13 02:17:48 +0300

    add

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import user from '../components/Profile/user.json'
+import {Profile} from '../Profile/Profile'
+
+console.log(user);
+
+export const App = () => {
+  return (
+    <>
+    <Profile username={user.username} tag={user.tag} location={user.location} avatar={user.avatar} stats={user.stats} />
+      
+    </>
+  );
+};
---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled'
+
+export const Conteiner = styled.div`
+ 
+
+`;
